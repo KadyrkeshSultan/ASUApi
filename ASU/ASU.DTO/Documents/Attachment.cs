583f0e6ba7980022828968da4bcb491fadab0f17
Sultan Kadyrkesh <sultankadyr@outlook.com>
2018-06-28 23:43:30 +0600

    комментарии к коду

---
+++ ASU/ASU.DTO/Documents/Attachment.cs
@@ -0,0 +1,19 @@
+﻿using ASU.DTO.Entities;
+
+namespace ASU.DTO.Documents
+{
+    /// <summary>
+    /// Вложение (файл)
+    /// </summary>
+    public class Attachment : BaseDTO
+    {
+        public string File { get; set; }
+        public byte[] FileData { get; set; }
+        public string Name { get; set; }
+
+        public Attachment()
+        {
+
+        }
+    }
+}
